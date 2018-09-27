e4e617f66852f6c8201776d7b7175e08ad7de8ce
demosdemosthenous3 <43623609+demosdemosthenous3@users.noreply.github.com>
2018-09-27 22:19:14 +0300

    Implemented LoginPage (#1)
    * Implemented LoginPage (xml+functionality) - missing role logic
    
    * Fixes

---
+++ app/views/login-page/login-page.js
@@ -0,0 +1,69 @@
+/*
+In NativeScript, a file with the same name as an XML file is known as
+a code-behind file. The code-behind is a great place to place your view
+logic, and to set up your page’s data binding.
+*/
+
+const dialogsModule = require("ui/dialogs");
+const frameModule = require("ui/frame");
+
+const UserLoginViewModel = require("../shared/user-login-model");
+const user = new UserLoginViewModel();
+
+function onNavigatingTo(args) {
+    const page = args.object;
+    isLoggingIn = user.isLoggingIn;
+    page.bindingContext = user;
+    page.actionBarHidden=true;
+}
+
+function toggleDisplay(){
+    isLoggingIn = !isLoggingIn;
+    user.set("isLoggingIn",isLoggingIn);
+}
+
+function login(){
+    user.login()
+        .catch(function(error){
+            console.log(error);
+            dialogsModule.alert({
+                message:"Unfortunately we could not find your account.",
+                okButtonText:"OK"
+            });
+            return Promise.reject();
+            
+        })
+        .then(function(){
+            //FIXME
+            //frameModule.topmost().navigate("views/"+viewToShow+"/"+viewToShow);         
+        })
+
+}
+
+function signUp(){
+    user.register()
+        .then(function(){
+            dialogsModule.alert({
+                message:"Your account was successfully created.",
+                okButtonText:"OK"
+            });
+        })
+        .catch(function(error){
+            dialogs.alert({
+                message:"Unfortunately we were unable to create your account.",
+                okButtonText:"OK"
+            });
+        });
+}
+
+exports.submit = function () {
+    if (isLoggingIn) {
+        login();
+    } else {
+        signUp();
+    }
+};
+
+exports.onNavigatingTo = onNavigatingTo;
+
+exports.toggleDisplay = toggleDisplay;
---
+++ app/views/shared/config.js
@@ -0,0 +1,10 @@
+module.exports ={
+    apiUrl: "https://baas.kinvey.com/",
+    apiHostname: 'CUSTDEP_HOST',
+    micHostname: 'CUSTDEP_MIC_HOST',
+    appKey: 'kid_HJmCXHtYQ',
+    appSecret: 'b8f781d3b99a473ab41a688fd6d25b28',
+    appUserHeader:'Basic a2lkX0hKbUNYSHRZUTpiOGY3ODFkM2I5OWE0NzNhYjQxYTY4OGZkNmQyNWIyOA==',
+    apiVersion: 3
+  }
+
