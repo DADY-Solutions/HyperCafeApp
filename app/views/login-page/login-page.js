89f747d65de93f17c4b8290a05d9242b150f7f13
demosdemosthenous3 <43623609+demosdemosthenous3@users.noreply.github.com>
2018-09-30 09:33:27 +0300

    Implmented role logic (#3)
    * Implmented role logic
    
    * eslint
    
    * bugfix

--- app/views/login-page/login-page.js
+++ app/views/login-page/login-page.js
@@ -4,66 +4,65 @@ a code-behind file. The code-behind is a great place to place your view
 logic, and to set up your page’s data binding.
 */
 
-const dialogsModule = require("ui/dialogs");
-const frameModule = require("ui/frame");
+const frameModule = require('ui/frame')
+const constants = require('../shared/constants')
+const Toast = require('nativescript-toast')
 
-const UserLoginViewModel = require("../shared/user-login-model");
-const user = new UserLoginViewModel();
+const UserLoginViewModel = require('../shared/user-login-model')
+const user = new UserLoginViewModel()
 
 function onNavigatingTo(args) {
-    const page = args.object;
-    isLoggingIn = user.isLoggingIn;
-    page.bindingContext = user;
-    page.actionBarHidden=true;
+  const page = args.object
+  page.bindingContext = user
+  page.actionBarHidden=true
 }
 
-function toggleDisplay(){
-    isLoggingIn = !isLoggingIn;
-    user.set("isLoggingIn",isLoggingIn);
+function toggleDisplay() {
+  user.isLoggingIn = !user.isLoggingIn
+  user.set('user.isLoggingIn',user.isLoggingIn)
 }
 
-function login(){
-    user.login()
-        .catch(function(error){
-            console.log(error);
-            dialogsModule.alert({
-                message:"Unfortunately we could not find your account.",
-                okButtonText:"OK"
-            });
-            return Promise.reject();
-            
-        })
-        .then(function(){
-            //FIXME
-            //frameModule.topmost().navigate("views/"+viewToShow+"/"+viewToShow);         
-        })
-
+function login() {
+  user.login()
+    .catch(function(error) {
+      console.log(error) // eslint-disable-line no-console
+      //TODO print the actual error
+      //undefined when using error.description or error._bodyTextDescription!!!
+      Toast.makeText('Please use the correct credentials').show()
+      return Promise.reject()
+    })
+    .then(function() {
+      let viewToShow
+      if (user.role === constants.customerRole) {
+        viewToShow = constants.customerView
+      } else {
+        viewToShow = constants.adminView
+      }
+      frameModule.topmost().navigate(viewToShow)
+    })
 }
 
-function signUp(){
-    user.register()
-        .then(function(){
-            dialogsModule.alert({
-                message:"Your account was successfully created.",
-                okButtonText:"OK"
-            });
-        })
-        .catch(function(error){
-            dialogs.alert({
-                message:"Unfortunately we were unable to create your account.",
-                okButtonText:"OK"
-            });
-        });
+function signUp() {
+  user.register()
+    .then(function() {
+      Toast.makeText('Your account was successfully created').show()
+    })
+    .catch(function(error) {
+      console.log(error) // eslint-disable-line no-console
+      //TODO print the actual error
+      //undefined when using error.description or error._bodyTextDescription!!!
+      Toast.makeText('We were unable to create your account').show()
+    })
 }
 
-exports.submit = function () {
-    if (isLoggingIn) {
-        login();
-    } else {
-        signUp();
-    }
-};
+exports.submit = function() {
+  if (user.isLoggingIn) {
+    login()
+  } else {
+    signUp()
+  }
+}
 
-exports.onNavigatingTo = onNavigatingTo;
+exports.onNavigatingTo = onNavigatingTo
 
-exports.toggleDisplay = toggleDisplay;
+exports.toggleDisplay = toggleDisplay
---
+++ app/views/shared/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+  //Role constants
+  customerRole: 'Customer',
+  adminRole: 'Admin',
+
+  //View constants
+  customerView: 'views/customerView/customerView',
+  adminView: 'views/adminView/adminView'
+}
