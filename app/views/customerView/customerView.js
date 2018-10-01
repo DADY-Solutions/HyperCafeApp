615f5e211b1b8393a36748018db7a16a82816dc4
Alexandros Yiangou <41811731+alexyiangou@users.noreply.github.com>
2018-10-01 15:03:03 +0300

    Navigation-Drawer-For-Clients-Screen (#4)
    * Install packages
    
    * Navigation Drawer
    
    * Added navigation drawer in clients screen
    
    * Added hamburger menu icons
    
    * Fix

---
+++ app/views/customerView/customerView.js
@@ -0,0 +1,17 @@
+const app = require('tns-core-modules/application')
+
+
+const HomeViewModel = require('./customerViewModel')
+
+function onNavigatingTo(args) {
+  const page = args.object
+  page.bindingContext = new HomeViewModel()
+}
+
+function onDrawerButtonTap(args) {
+  const sideDrawer = app.getRootView()
+  sideDrawer.showDrawer()
+}
+
+exports.onDrawerButtonTap = onDrawerButtonTap
+exports.onNavigatingTo = onNavigatingTo
