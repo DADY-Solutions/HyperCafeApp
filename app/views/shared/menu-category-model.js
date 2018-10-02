10d0399dccad791276b2c0c84a2383fe894e7352
Demos Demosthenous <demosdemosthenous3@gmail.com>
2018-10-02 17:40:36 +0300

    Created menuView for admin

--- app/views/shared/menu-category-model.js
+++ app/views/shared/menu-category-model.js
@@ -1,26 +1,57 @@
-const observableModule = require('data/observable')
-const observableArray = require('data/observable').observableArray
+
+const ObservableArray = require('data/observable-array').ObservableArray
 const fetchModule = require('fetch')
 const config = require('../shared/config')
 const requestUtils = require('../../utils/requestUtils')
 
+let viewModel
+let baseUrl
+function MenuCategoryModel(categories) {
+  baseUrl = config.apiUrl + 'appdata/' + config.appKey + '/MenuCategories'
+  viewModel =  new ObservableArray(categories)
 
-function MenuCategoryModel(menuItems) {
-  const viewModel = observableModule.fromObject({
-    categoryName: 'Hot Drinks',
-    items: new observableArray(menuItems)
-  })
-
-  viewModel.addCategory = function() {
-    return fetchModule.fetch(config.apiUrl + 'appdata/' + config.appKey + '/MenuCategory',{
+  viewModel.addCategory = function(category) {
+    return fetchModule.fetch(baseUrl,{
       method: 'POST',
       body: JSON.stringify({
-        categoryName: viewModel.get('categoryName'),
+        categoryName: category,
       }),
-      headers: requestUtils.getCommonHeaders()
-    })
+      headers: requestUtils.getCommonHeadersAfterLogin()
+    }).then(requestUtils.handleErrors)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        viewModel.push({categoryName: category.categoryName, id: data._id})
+      })
   }
+
+  viewModel.load = function() {
+    return fetchModule.fetch(baseUrl, {
+      method: 'GET',
+      headers: requestUtils.getCommonHeadersAfterLogin()
+    })
+      .then(function(response) {
+        return response.json()
+      }).then(function(data) {
+        data.forEach(function(category) {
+          viewModel.push({
+            categoryName: category.categoryName,
+            id: category._id
+          })
+        })
+      })
+  }
+  viewModel.empty = function() {
+    while (viewModel.length) {
+      viewModel.pop()
+    }
+  }
+
   return viewModel
+
 }
+
+
 
 module.exports = MenuCategoryModel
